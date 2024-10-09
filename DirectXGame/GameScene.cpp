7212d32b474bf9e34af48e159e3ebe407e3d976e
LC1D_17_Chiba_Daichi <146914435+daichChiba@users.noreply.github.com>
2024-10-09 15:15:00 +0900

    Engineを追加した。

---
+++ DirectXGame/GameScene.cpp
@@ -0,0 +1,78 @@
+#include "GameScene.h"
+#include <cassert>
+
+using namespace KamataEngine ;
+// インストラクタ
+GameScene::GameScene() {}
+// デストラクタ
+GameScene::~GameScene() {
+
+	// 3Dモデルデータの解放
+	delete model_;
+}
+
+void GameScene::Initialize() {
+
+	//// ファイル名を指定してテクスチャを読み込む
+	//textureHandle_ = TextureManager::Load("mario.jpg");
+
+	dxCommon_ = DirectXCommon::GetInstance();
+	input_ = Input::GetInstance();
+	audio_ = Audio::GetInstance();
+
+	// 3Dモデルデータの生成
+	model_ = Model::Create();
+	// ビュープロジェクションの初期化
+	camera_.Initialize();
+
+}
+
+void GameScene::Update() {
+}
+
+void GameScene::Draw() {
+
+	// コマンドリストの取得
+	ID3D12GraphicsCommandList* commandList = dxCommon_->GetCommandList();
+
+#pragma region 背景スプライト描画
+	// 背景スプライト描画前処理
+	Sprite::PreDraw(commandList);
+
+	/// <summary>
+	/// ここに背景スプライトの描画処理を追加できる
+	/// </summary>
+
+	// スプライト描画後処理
+	Sprite::PostDraw();
+	// 深度バッファクリア
+	dxCommon_->ClearDepthBuffer();
+#pragma endregion
+
+#pragma region 3Dオブジェクト描画
+	// 3Dオブジェクト描画前処理
+	Model::PreDraw(commandList);
+
+	/// <summary>
+	/// ここに3Dオブジェクトの描画処理を追加できる
+	/// </summary>
+
+
+	// 3Dオブジェクト描画後処理
+	Model::PostDraw();
+#pragma endregion
+
+#pragma region 前景スプライト描画
+	// 前景スプライト描画前処理
+	Sprite::PreDraw(commandList);
+
+	/// <summary>
+	/// ここに前景スプライトの描画処理を追加できる
+	/// </summary>
+
+	// スプライト描画後処理
+	Sprite::PostDraw();
+
+#pragma endregion
+}
+
